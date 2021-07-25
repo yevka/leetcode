0f363f3dacee5e3574bb6b53bb369d82b91d4d64
eot42 <eolitio@gmail.com>
2021-07-25 18:48:22 +0300

    add build skeleton to 1920. Build Array from Permutation

---
+++ build-array-from-permutation.cc
@@ -0,0 +1,19 @@
+//
+// Created by eolitio on 25.07.21.
+//
+
+#include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> buildArray(vector<int>& nums) {
+        return {};
+    }
+};
+
+TEST(Solution, twoSum) {
+    ASSERT_TRUE(1, 1);
+}
