7238db5bd185b80984eb30a490e2c1faeb4d1652
eot42 <eolitio@gmail.com>
2021-07-27 00:25:54 +0300

    fix 7. Reverse Integer

--- reverse-integer.cc
+++ reverse-integer.cc
@@ -4,16 +4,23 @@
 #include <gtest/gtest.h>
 
 #include <algorithm>
+#include <cstdint>
 #include <string>
+#include <iostream>
+#include <climits>
 
 using namespace std;
 
 class Solution {
 public:
     int reverse(int x) {
-        string str = to_string(x);
+        string str = to_string(abs(x));
         std::reverse(str.begin(), str.end());
-        return x < 0 ? stoi(str) * -1 : stoi(str);
+        int64_t x64 = stoll(str);
+        if (x64 > INT_MAX || x64 < INT_MIN) {
+            return 0;
+        }
+        return x < 0 ? x64 * -1: x64;
     }
 };
 
@@ -23,4 +30,5 @@ TEST(Solution, reverse) {
     ASSERT_EQ(solution.reverse(-123), -321);
     ASSERT_EQ(solution.reverse(120), 21);
     ASSERT_EQ(solution.reverse(0), 0);
+    ASSERT_EQ(solution.reverse(1534236469), 0);
 }
